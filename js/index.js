1ad6dc5fbd71019e774ee71bb13ede2fe8774b28
namlunberg <ostashev.egor@gmail.com>
2021-11-23 13:44:09 +0300

    №2

--- js/index.js
+++ js/index.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
         $(this).toggleClass('active');
         $(this).parent().find('.info-bottom').slideToggle();
     });
+
+    $('.filter-top').on('click', function () {
+        $(this).toggleClass('active');
+        $('.filter-bottom').slideToggle();
+    });
+
+    $('.priority-top').on('click', function () {
+        $(this).toggleClass('active');
+        $('.priority-bottom').slideToggle();
+    });
+
+    $('.priority-bottom-head__img').on('click', function () {
+        $(this).toggleClass('active');
+        $('.priority-bottom').slideToggle();
+    });
 });
